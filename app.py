5e5e07f6fa5fd6a17973dcfe61c66ae696049a72
yoavbuzali21 <yoavb2105@gmail.com>
2026-09-18 10:53:59 +0300

    Add NBA point prediction feature using Streamlit

--- app.py
+++ app.py
@@ -1,1 +1,8 @@
+import streamlit as st
 
+st.title("NBA point prediction")
+height=st.number_input("Predict a height: ")
+w=-0.03650734467294015
+b=15.534315785648516
+prediction=w*height+b
+st.write(f"The prediction is {prediction}")
